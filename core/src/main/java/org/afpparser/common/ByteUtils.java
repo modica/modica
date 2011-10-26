9569a08d7f57d5a6ebc4c3b6033c24cc449b85d3
mhoushmand <mhoushmand@thunderhead.com>
2011-10-26 12:16:30 +0100

    Separated ByteUtils to big/little endian components

--- core/src/main/java/org/afpparser/common/ByteUtils.java
+++ core/src/main/java/org/afpparser/common/ByteUtils.java
@@ -1,23 +1,41 @@
 package org.afpparser.common;
 
-public class ByteUtils {
+public abstract class ByteUtils {
+
+    private static final ByteUtils littleEndianUtils = new LittleEndianByteUtils();
+    private static final ByteUtils bigEndianUtils = new BigEndianByteUtils();
 
     private ByteUtils() {
     }
 
-    public static int bytesToInt(byte[] bytes, int position, int numberOfBytes) {
+    public abstract int bytesToInt(byte[] bytes, int position, int length);
+    
+    public abstract int bytesToInt(byte[] bytes);
+
+    private static final int bytesToInt(byte[] bytes, int position, int length,
+            int bitShiftStart, int bitShitIncrement) {
         int num = 0;
-        if (numberOfBytes > 4) {
+        if (length > 4) {
             throw new IllegalArgumentException("The maximum capacity for an int is 32 bytes");
         }
-        for (int i = 0, bitShift = (numberOfBytes - 1) * 8; i < numberOfBytes; i++, bitShift -= 8) {
+        for (int i = 0, bitShift = bitShiftStart; i < length; i++, bitShift += bitShitIncrement) {
             num += (bytes[position + i] & 0xff) << bitShift;
         }
         return num;
     }
 
-    public static int bytesToInt(byte[] bytes) {
-        return bytesToInt(bytes, 0, bytes.length);
+    public static final byte[] hexToBytes(String hex, int position, int length) {
+        byte[] data = new byte[length / 2];
+        char[] hexArray = hex.toCharArray();
+        for (int i = position; i < length; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(hexArray[i], 16) << 4)
+                    + (Character.digit(hexArray[i + 1], 16)));
+        }
+        return data;
+    }
+
+    public static final byte[] hexToBytes(String hex) {
+        return hexToBytes(hex, 0, hex.length());
     }
 
     public static byte[] createByteArray(int... arg) {
@@ -26,5 +44,45 @@ public class ByteUtils {
             byteArray[i] = (byte) arg[i];
         }
         return byteArray;
+    }
+
+    private static class LittleEndianByteUtils extends ByteUtils {
+
+        private LittleEndianByteUtils() {
+        }
+
+        public int bytesToInt(byte[] bytes, int position, int length) {
+            return bytesToInt(bytes, position, length, (length - 1) * 8, -8);
+        }
+
+        public int bytesToInt(byte[] bytes) {
+            return bytesToInt(bytes, 0, bytes.length);
+        }
+
+    }
+
+    private static class BigEndianByteUtils extends ByteUtils {
+
+        private BigEndianByteUtils() {
+        }
+
+        public int bytesToInt(byte[] bytes, int position, int length) {
+            return bytesToInt(bytes, position, length, 0, 8);
+        }
+
+        public int bytesToInt(byte[] bytes) {
+            return bytesToInt(bytes, 0, bytes.length);
+        }
+
+    }
+
+
+
+    public static ByteUtils newLittleEndianUtils() {
+        return littleEndianUtils;
+    }
+
+    public static ByteUtils newBigEndianUtils() {
+        return bigEndianUtils;
     }
 }
--- core/src/test/java/org/afpparser/common/ByteUtilsTestCase.java
+++ core/src/test/java/org/afpparser/common/ByteUtilsTestCase.java
@@ -2,26 +2,74 @@ package org.afpparser.common;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class ByteUtilsTestCase {
 
+    private ByteUtils littleEndianSut = ByteUtils.newLittleEndianUtils();
+    private ByteUtils bigEndianSut = ByteUtils.newBigEndianUtils();
+
+    private byte[] oneToSeven;
+
+    @Before
+    public void setUp() {
+        oneToSeven = ByteUtils.createByteArray(1, 2, 3, 4, 5, 6, 7);
+    }
+
     @Test
-    public void testBytesToInt() {
+    public void testLittleEndianBytesToInt() {
         byte[] bytes = ByteUtils.createByteArray(0x01);
-        assertEquals(0x01, ByteUtils.bytesToInt(bytes, 0, 1));
+        assertEquals(0x01, littleEndianSut.bytesToInt(bytes, 0, 1));
+        assertEquals(0x01, littleEndianSut.bytesToInt(bytes));
 
         bytes = ByteUtils.createByteArray(0x01, 0x02);
-        assertEquals(0x102, ByteUtils.bytesToInt(bytes, 0, 2));
+        assertEquals(0x102, littleEndianSut.bytesToInt(bytes, 0, 2));
+        assertEquals(0x102, littleEndianSut.bytesToInt(bytes));
 
         bytes = ByteUtils.createByteArray(0x01, 0x02, 0x03);
-        int actual = ByteUtils.bytesToInt(bytes, 0, 3);
-        assertEquals(0x10203, actual);
+        assertEquals(0x10203, littleEndianSut.bytesToInt(bytes, 0, 3));
+        assertEquals(0x10203, littleEndianSut.bytesToInt(bytes));
+
+        assertEquals(0x1020304, littleEndianSut.bytesToInt(oneToSeven, 0, 4));
+        testBytesToIntFailure("A 7 byte number is not legal", littleEndianSut, oneToSeven);
+    }
+
+    private void testBytesToIntFailure(String msg, ByteUtils sut, byte[] bytes) {
+        try {
+            sut.bytesToInt(bytes);
+            fail(msg);
+        } catch (IllegalArgumentException e) {
+            // Pass
+        }
+
+        try {
+            sut.bytesToInt(bytes, 0, bytes.length);
+            fail(msg);
+        } catch (IllegalArgumentException e) {
+            // Pass
+        }
     }
     
     @Test
-    public void testCreateByteArray() {
+    public void testBigEndianBytesToInt() {
+        byte[] bytes = ByteUtils.createByteArray(0x01);
+        assertEquals(0x01, bigEndianSut.bytesToInt(bytes, 0, 1));
+        assertEquals(0x01, bigEndianSut.bytesToInt(bytes));
+
+        bytes = ByteUtils.createByteArray(0x01, 0x02);
+        assertEquals(0x201, bigEndianSut.bytesToInt(bytes, 0, 2));
+        assertEquals(0x201, bigEndianSut.bytesToInt(bytes));
+
+        bytes = ByteUtils.createByteArray(0x01, 0x02, 0x03);
+        assertEquals(0x30201, bigEndianSut.bytesToInt(bytes, 0, 3));
+        assertEquals(0x30201, bigEndianSut.bytesToInt(bytes));
+    }
+
+    @Test
+    public void testCommonCreateByteArray() {
         byte[] array = ByteUtils.createByteArray(1, 2, 3, 4, 5, 6, 7);
         byte[] expected = new byte[] {
                 (byte)1,
@@ -37,4 +85,10 @@ public class ByteUtilsTestCase {
         expected = new byte[0];
         assertArrayEquals(expected, array);
     }
+
+    @Test
+    public void testCommonHexToBytes() {
+        byte[] expected = ByteUtils.createByteArray(1, 2, 3, 4, 5, 6);
+    }
+
 }
