d7912055ff4a44d40ead6844cf16f8447395dd89
Mehdi Houshmand <med1985@gmail.com>
2011-10-30 14:35:39 +0000

    Bloody serialization in Exception...

--- core/src/main/java/org/afpparser/parser/InvalidAfpException.java
+++ core/src/main/java/org/afpparser/parser/InvalidAfpException.java
@@ -1,6 +1,8 @@
 package org.afpparser.parser;
 
 public class InvalidAfpException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
     public InvalidAfpException(String msg, Throwable e) {
         super(msg, e);
     }
