4098a712cca089376b89e355baac0b2b0718c337
Mehdi Houshmand <med1985@gmail.com>
2011-10-30 14:35:15 +0000

    Improved command line options

--- core/src/main/java/org/afpparser/Main.java
+++ core/src/main/java/org/afpparser/Main.java
@@ -3,31 +3,42 @@ package org.afpparser;
 import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.OptionGroup;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
 public class Main {
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) {
         CommandLineParser cliParser = new BasicParser();
-
         Options opts = createCliOptions();
 
-        CommandLine cmd = cliParser.parse(opts, args);
-        if (cmd.hasOption('p')) {
-            AFPParser.main(cmd.getOptionValue('p'));
-        } else if (cmd.hasOption('h')) {
-            System.out.println("here");
+        try {
+            CommandLine cmd = cliParser.parse(opts, args);
+            if (cmd.hasOption('p')) {
+                AFPParser.main(cmd.getOptionValue('p'));
+            } else if (cmd.hasOption('h') || args.length == 0) {
+                printHelp(opts);
+            }
+        } catch (ParseException pe) {
+            System.out.println("Unexpected exception: " + pe.getMessage());
         }
     }
 
-    public static Options createCliOptions() {
+    private static void printHelp(Options opts) {
+        HelpFormatter helpFormatter = new HelpFormatter();
+        helpFormatter.printHelp("afpparser", opts);
+    }
+
+    private static Options createCliOptions() {
         final Options opts = new Options();
         opts.addOption("h", "help", false, "Print usage information");
         OptionGroup optGroup = new OptionGroup();
-        optGroup.addOption(new Option("p", "parse", true, "Parse an AFP document"));
-        optGroup.addOption(new Option("c", "compare", true, "Compare two AFP documents"));
+        optGroup.addOption(new Option("p", "parse", true, "Parse an AFP document and print the"
+                + " structured field data to the commandline."));
+        optGroup.addOption(new Option("c", "compare", true, "Compare two AFP documents and print"
+                + " any differences to the command line."));
         opts.addOptionGroup(optGroup);
         return opts;
     }
