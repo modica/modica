49dfe78b1320473cab277dc48587cc823cbbcaa2
Peter Hancock <peter.hancock@gmail.com>
2012-03-09 07:27:20 +0000

    Filter for afp model session

--- web/src/main/java/org/modica/web/WicketApplication.java
+++ web/src/main/java/org/modica/web/WicketApplication.java
@@ -1,6 +1,9 @@
 package org.modica.web;
 
+import org.apache.wicket.Session;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.request.cycle.AbstractRequestCycleListener;
+import org.apache.wicket.request.cycle.RequestCycle;
 import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
 
 /**
@@ -18,5 +21,15 @@ public class WicketApplication extends WebApplication {
     public void init() {
         super.init();
         getComponentInstantiationListeners().add(new SpringComponentInjector(this));
+
+        getRequestCycleListeners().add(new AbstractRequestCycleListener() {
+            @Override
+            public void onBeginRequest(RequestCycle cycle) {
+                System.out.println(Session.get().getId());
+                super.onBeginRequest(cycle);
+            }
+
+        });
     }
+
 }
---
+++ web/src/main/java/org/modica/web/model/AfpService.java
@@ -0,0 +1,8 @@
+package org.modica.web.model;
+
+public class AfpService {
+
+    public void closeSession() {
+        // close file / fileChannel
+    }
+}
