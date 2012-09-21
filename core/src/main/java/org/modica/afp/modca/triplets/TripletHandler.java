7ceb68a94d645800244ea1c1fb8f765ec810db64
Mehdi Houshmand <med1985@gmail.com>
2012-09-21 09:06:04 +0100

    Improved the method by which triplets are parsed

--- core/src/main/java/org/modica/afp/modca/triplets/TripletHandler.java
+++ core/src/main/java/org/modica/afp/modca/triplets/TripletHandler.java
@@ -7,8 +7,6 @@ import java.util.List;
 
 import org.modica.afp.modca.Context;
 import org.modica.afp.modca.Parameters;
-import org.modica.afp.modca.triplets.foca.ResourceManagement;
-import org.modica.afp.modca.triplets.fullyqualifiedname.FullyQualifiedName;
 
 /**
  * A handler for parsing Structured Field Triplets and Triplet groups.
@@ -36,40 +34,7 @@ public final class TripletHandler {
         while (params.getPosition() < position + length) {
             int tripletLength = (int) params.getUInt(1);
             TripletIdentifiers tId = TripletIdentifiers.getTripletId(params.getByte());
-            switch (tId) {
-            case coded_graphic_character_set_global_identifier:
-                tripletList.add(Cgcsgid.parse(params, context));
-                break;
-            case character_rotation:
-                tripletList.add(new CharacterRotation(params));
-                break;
-            case descriptor_position:
-                tripletList.add(new DescriptorPosition(params.getByte()));
-                break;
-            case fully_qualified_name:
-                tripletList.add(FullyQualifiedName.parse(params, tripletLength));
-                break;
-            case mapping_option:
-                tripletList.add(new MappingOption(params.getByte()));
-                break;
-            case measurement_units:
-                tripletList.add(new MeasurementUnits(params));
-                break;
-            case object_area_size:
-                tripletList.add(new ObjectAreaSize(params));
-                break;
-            case object_function_set_specification:
-                tripletList.add(new ObjectFunctionSetSpecification(params, tripletLength));
-                break;
-            case resource_local_identifier:
-                tripletList.add(new ResourceLocalId(params));
-                break;
-            case resource_management:
-                tripletList.add(ResourceManagement.parse(params));
-                break;
-            default:
-                params.skip(tripletLength - 2);
-            }
+            tripletList.add(tId.buildTriplet(tripletLength, params, context));
         }
         return tripletList;
     }
--- core/src/main/java/org/modica/afp/modca/triplets/TripletIdentifiers.java
+++ core/src/main/java/org/modica/afp/modca/triplets/TripletIdentifiers.java
@@ -1,70 +1,370 @@
 package org.modica.afp.modca.triplets;
 
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import org.modica.afp.modca.Context;
+import org.modica.afp.modca.ParameterAsString;
+import org.modica.afp.modca.Parameters;
+import org.modica.afp.modca.triplets.foca.ResourceManagement;
+import org.modica.afp.modca.triplets.fullyqualifiedname.FullyQualifiedName;
 
 /**
  * Enumeration of the structured field triplet identifiers.
  */
 public enum TripletIdentifiers {
-    coded_graphic_character_set_global_identifier(0x01),
-    fully_qualified_name(0x02),
-    mapping_option(0x04),
-    object_classification(0x10),
-    modca_interchange_set(0x18),
-    font_descriptor_specification(0x1f),
-    coded_graphic_character_set_global_identifier1(0x20),
-    object_function_set_specification(0x21),
-    extended_resource_local_identifier(0x22),
-    resource_local_identifier(0x24),
-    resource_section_number(0x25),
-    character_rotation(0x26),
-    object_byte_offset(0x2d),
-    attribute_value(0x36),
-    descriptor_position(0x43),
-    media_eject_control(0x45),
-    page_overlay_conditional_processing(0x46),
-    resource_usage_attribute(0x47),
-    measurement_units(0x4b),
-    object_area_size(0x4c),
-    area_definition(0x4d),
-    color_specification(0x4e),
-    encoding_scheme_id(0x50),
-    medium_map_page_number(0x56),
-    object_byte_extent(0x57),
-    object_structured_field_offset(0x58),
-    object_structured_field_extent(0x59),
-    object_offset(0x5a),
-    font_horizontal_scale_factor(0x5d),
-    object_count(0x5e),
-    object_date_and_timestamp(0x62),
-    resource_management(0x63),
-    object_origin_identifier(0x64),
-    comment(0x65),
-    medium_orientation(0x68),
-    resource_object_include(0x6c),
-    presentation_space_reset_mixing(0x70),
-    presentation_space_mixing_rule(0x71),
-    universal_date_and_timestamp(0x72),
-    toner_saver(0x74),
-    color_fidelity(0x75),
-    font_fidelity(0x78),
-    attribute_qualifier(0x80),
-    page_position_information(0x81),
-    parameter_value(0x82),
-    presentation_control(0x83),
-    font_resolution_and_metric_technology(0x84),
-    finishing_operation(0x85),
-    text_fidelity(0x86),
-    media_fidelity(0x87),
-    finishing_fidelity(0x88),
-    data_object_font_descriptor(0x8b),
-    locale_selector(0x8c),
-    up3i_finishing_operation(0x8e),
-    color_management_resource_descriptor(0x91),
-    rendering_intent(0x95),
-    cmr_tag_fidelity(0x96),
-    device_appearance(0x97);
+    coded_graphic_character_set_global_identifier(0x01) {
+        @Override
+        Triplet buildTriplet(int length, Parameters params, Context context) {
+            return Cgcsgid.parse(params, context);
+        }
+    },
+    fully_qualified_name(0x02) {
+        @Override
+        Triplet buildTriplet(int length, Parameters params, Context context)
+                throws UnsupportedEncodingException, MalformedURLException {
+            return FullyQualifiedName.parse(params, length);
+        }
+    },
+    mapping_option(0x04) {
+        @Override
+        Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new MappingOption(params.getByte());
+        }
+    },
+    object_classification(0x10) {
+        @Override
+        Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    modca_interchange_set(0x18) {
+        @Override
+        Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    font_descriptor_specification(0x1f) {
+        @Override
+        Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    coded_graphic_character_set_global_identifier1(0x20) {
+        @Override
+        Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    object_function_set_specification(0x21) {
+        @Override
+        Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new ObjectFunctionSetSpecification(params, length);
+        }
+    },
+    extended_resource_local_identifier(0x22) {
+        @Override
+        Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    resource_local_identifier(0x24) {
+        @Override
+        Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new ResourceLocalId(params);
+        }
+    },
+    resource_section_number(0x25) {
+        @Override
+        Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    character_rotation(0x26) {
+        @Override
+        Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new CharacterRotation(params);
+        }
+    },
+    object_byte_offset(0x2d) {
+        @Override
+        Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    attribute_value(0x36) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    descriptor_position(0x43) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new DescriptorPosition(params.getByte());
+        }
+    },
+    media_eject_control(0x45) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    page_overlay_conditional_processing(0x46) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    resource_usage_attribute(0x47) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    measurement_units(0x4b) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new MeasurementUnits(params);
+        }
+    },
+    object_area_size(0x4c) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new ObjectAreaSize(params);
+        }
+    },
+    area_definition(0x4d) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    color_specification(0x4e) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    encoding_scheme_id(0x50) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    medium_map_page_number(0x56) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    object_byte_extent(0x57) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    object_structured_field_offset(0x58) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    object_structured_field_extent(0x59) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    object_offset(0x5a) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    font_horizontal_scale_factor(0x5d) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    object_count(0x5e) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    object_date_and_timestamp(0x62) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    resource_management(0x63) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return ResourceManagement.parse(params);
+        }
+    },
+    object_origin_identifier(0x64) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    comment(0x65) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    medium_orientation(0x68) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    resource_object_include(0x6c) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    presentation_space_reset_mixing(0x70) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    presentation_space_mixing_rule(0x71) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    universal_date_and_timestamp(0x72) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    toner_saver(0x74) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    color_fidelity(0x75) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    font_fidelity(0x78) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    attribute_qualifier(0x80) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    page_position_information(0x81) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    parameter_value(0x82) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    presentation_control(0x83) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    font_resolution_and_metric_technology(0x84) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    finishing_operation(0x85) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    text_fidelity(0x86) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    media_fidelity(0x87) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    finishing_fidelity(0x88) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    data_object_font_descriptor(0x8b) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    locale_selector(0x8c) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    up3i_finishing_operation(0x8e) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    color_management_resource_descriptor(0x91) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    rendering_intent(0x95) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    cmr_tag_fidelity(0x96) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    },
+    device_appearance(0x97) {
+        @Override
+        public Triplet buildTriplet(int length, Parameters params, Context context) {
+            return new NotYetImplementedTriplet(length, this, params);
+        }
+    };
 
     private static final int MAX_LENGTH = 254;
 
@@ -84,19 +384,59 @@ public enum TripletIdentifiers {
         this.name = this.toString();
     }
 
-    public byte getId() {
+    byte getId() {
         return id;
     }
 
-    public String getName() {
+    String getName() {
         return name;
     }
 
-    public static int getmaxlength() {
+    public static int getMaxLength() {
         return MAX_LENGTH;
     }
 
-    public static TripletIdentifiers getTripletId(byte tId) {
+    static TripletIdentifiers getTripletId(byte tId) {
         return CACHE.get(tId);
+    }
+
+    abstract Triplet buildTriplet(int length, Parameters params, Context context)
+            throws UnsupportedEncodingException, MalformedURLException;
+
+    public static final class NotYetImplementedTriplet extends Triplet {
+
+        private final int length;
+        private final TripletIdentifiers tId;
+
+        private NotYetImplementedTriplet(int length, TripletIdentifiers tId, Parameters params) {
+            this.length = length;
+            this.tId = tId;
+            params.skip(length - 2);
+        }
+
+        @Override
+        public int getLength() {
+            return length;
+        }
+
+        @Override
+        public TripletIdentifiers getTid() {
+            return tId;
+        }
+
+        @Override
+        public List<ParameterAsString> getParameters() {
+            return null;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            return this == o;
+        }
+
+        @Override
+        public int hashCode() {
+            return this.hashCode();
+        }
     }
 }
